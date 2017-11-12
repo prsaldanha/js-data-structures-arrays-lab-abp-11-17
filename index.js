62213a82ec6e2063982588e56d1883eeba34d4d8
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 07:37:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,3 +37,9 @@ function removeLastDriver()
   var newarray = drivers.slice(0, drivers.length - 1);
   return newarray;
 }
+
+function removeFirstDriver()
+{
+  var newarray = drivers.slice(1, drivers.length);
+  return newarray;
+}
